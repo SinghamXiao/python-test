6173463e4f4dc2ead7826f73e2c4a6d59aedf6a1
singham.yuan <singham.yuan@derbysoft.com>
2017-07-21 11:05:34 +0800

    modify build.py

--- build.py
+++ build.py
@@ -5,13 +5,15 @@ import pexpect
 import sys
 
 host = ""
+loginUser = ""
 loginPassword = ""
+COMMANDS = """ls -l && cd /usr/local/ && ls -l"""
 
 
-def scp_command(src, dest, password):
+def ssh_command(commands):
     ssh_newkey = 'Are you sure you want to continue connecting'
     # 为 ssh 命令生成一个 spawn 类的子程序对象.
-    child = pexpect.spawn('scp -r %s %s' % (src, dest))
+    child = pexpect.spawn(commands[0])
     i = child.expect([pexpect.TIMEOUT, ssh_newkey, 'password: '])
     # 如果登录超时，打印出错信息，并退出.
     if i == 0:  # Timeout
@@ -30,13 +32,20 @@ def scp_command(src, dest, password):
             print(child.before, child.after)
         return None
     # 输入密码.
-    child.sendline(password)
+    child.sendline(commands[1])
     return child
 
 
 if __name__ == "__main__":
-    print('This is main of module "build.py"')
-    child = scp_command(sys.argv[1], host, loginPassword)
+    print("Start ...")
+    command = 'scp -r %s %s' % (sys.argv[1], loginUser + "@" + host + ":~/")
+    child = ssh_command([command, loginPassword])
+    child.expect(pexpect.EOF)
+    # 输出命令结果.
+    print(child.before)
+
+    command = 'ssh -l %s %s %s' % (loginUser, host, COMMANDS)
+    child = ssh_command([command, loginPassword])
     child.expect(pexpect.EOF)
     # 输出命令结果.
     print(child.before)
